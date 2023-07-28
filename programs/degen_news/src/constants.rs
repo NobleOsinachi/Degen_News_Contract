9948a307b0fbf40b65ba190c22d3351481343854
alan-cousin <alan-cousin>
2023-07-28 14:25:38 +0900

    update

--- programs/degen_news/src/constants.rs
+++ programs/degen_news/src/constants.rs
@@ -2,8 +2,8 @@ use anchor_lang::prelude::Pubkey;
 
 pub const POOL_SEED: &str = "pool";
 
-pub const ADMIN_KEY: Pubkey = anchor_lang::solana_program::pubkey!("3ttYrBAp5D2sTG2gaBjg8EtrZecqBQSBuFRhsqHWPYxX"); 
-pub const SENIOR_KEY: Pubkey = anchor_lang::solana_program::pubkey!("4tPo5281MTr1D3vGhPot3zoJavicXiNfLX6i9RhYgWvA"); 
+pub const ADMIN_KEY: Pubkey = anchor_lang::solana_program::pubkey!("87rtQgi5oMXxZih7jEp6fWG1eGsJTnVb1FFAAa6joLKA"); 
+pub const SENIOR_KEY: Pubkey = anchor_lang::solana_program::pubkey!("C5gk2XeNundJqT9Y4cS13wX2eppyW3D1LCNka74ytNe9"); 
 
 pub const FIXED_SOL: u64 = 100000000;
 pub const DECIMAL: u64 = 1000000000;
